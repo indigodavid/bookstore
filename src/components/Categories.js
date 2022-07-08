694c0ae7c459f349eb033b1675bc7ab29a329772
David Vera Castillo <indigodavid@gmail.com>
2022-07-07 20:09:06 -0500

    Add Categories module

---
+++ src/components/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Categories = () => {
+  const button = <button type="button">Check status</button>;
+  return (
+    <div>
+      {button}
+    </div>
+  );
+};
+
+export default Categories;
