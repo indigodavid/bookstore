8f1b520080713885976d326b059f0f0dbf2a1b2d
David Vera Castillo <indigodavid@gmail.com>
2022-07-07 20:09:27 -0500

    Add Navbar module

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Books',
+    },
+    {
+      id: 2,
+      path: '/categories',
+      text: 'Categories',
+    },
+  ];
+
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <Link to="/">Bookstore CMS</Link>
+      </div>
+      <ul className="navLinks">
+        {links.map((link) => (
+          <li key={link.id}>
+            <NavLink to={link.path}>{link.text}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
