3f310fb117f7be52aa2a0d3ff34142831b46cfeb
David Vera Castillo <indigodavid@gmail.com>
2022-07-13 21:09:51 -0500

    Refactor BookList module using async method

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
-import { removeBook } from '../redux/books/books';
+import { asyncRemoveBook } from '../redux/books/booksAsyncActions';
 
 const BookList = () => {
   const books = useSelector((state) => state.books);
   const dispatch = useDispatch();
   const handleRemove = (book) => {
-    dispatch(removeBook(book));
+    dispatch(asyncRemoveBook(book)).then((response) => {
+      const alert = document.getElementById('alert');
+      alert.innerHTML = response.payload;
+      setTimeout(() => {
+        alert.innerHTML = '';
+      }, 3000);
+    });
   };
+
   return (
     <ul className="bookList">
       {books.map((book) => (
-        <li key={book.id}>
+        <li key={book.item_id}>
           <div className="bookWrapper">
-            <div className="genre">{book.genre}</div>
+            <div className="genre">{book.category}</div>
             <Book title={book.title} author={book.author} />
             <div className="bookActions">
-              <button className="comments" type="button" id={`comments-${book.id}`}>Comments</button>
-              <button className="remove" type="button" id={`remove-${book.id}`} onClick={() => handleRemove(book)}>Remove</button>
-              <button className="edit" type="button" id={`edit-${book.id}`}>Edit</button>
+              <button className="comments" type="button">Comments</button>
+              <button className="remove" type="button" onClick={() => handleRemove(book)}>Remove</button>
+              <button className="edit" type="button">Edit</button>
             </div>
           </div>
           <div className="progress">Progress Icon</div>
