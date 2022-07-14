5defe9ff55b3b8c09f82091af703c9bd9a49d05e
David Vera Castillo <indigodavid@gmail.com>
2022-07-13 20:59:37 -0500

    Add actions names in separate module

---
+++ src/redux/books/actions.js
@@ -0,0 +1,15 @@
+const ADD = 'bookstore/books/ADD';
+const UPDATE = 'bookstore/books/UPDATE';
+const REMOVE = 'bookstore/books/REMOVE';
+const GET = 'bookstore/books/GET';
+const ASYNC_ADD = 'bookstore/books/ASYNC_ADD';
+const ASYNC_REMOVE = 'bookstore/books/ASYNC_REMOVE';
+
+export {
+  ADD,
+  UPDATE,
+  REMOVE,
+  GET,
+  ASYNC_ADD,
+  ASYNC_REMOVE,
+};
